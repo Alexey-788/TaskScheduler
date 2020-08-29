1d18e3bbd47a749618130185219e6d1bc8aea8d2
Alex Sheblykin <alexey.sheblykin1@gmail.org>
2020-08-29 20:53:00 +0300

    init

---
+++ src/main/java/com/alex/TaskSchedulerApp.java
@@ -0,0 +1,13 @@
+package com.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class TaskSchedulerApp {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(TaskSchedulerApp.class, args);
+    }
+}
